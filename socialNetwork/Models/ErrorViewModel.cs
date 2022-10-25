bd40c8fd8f7e63ab07e3b0230c494f194513ba77
luisferllub230 <84060723+luisferllub230@users.noreply.github.com>
2022-10-24 21:08:32 -0400

    type: hello world and implement onion and pakages

---
+++ socialNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace socialNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
